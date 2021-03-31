1fcb75d43dcc772e680b622bd21847a3aafb2476
matthias-debaets <matthias.debaets@student.ucll.be>
2021-03-31 09:10:47 +0200

    Frontend (added banner and changed name)

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Banner from "vue-banner-better";
+// use plugin,it will mount Banner and slide components globally
+Vue.use(Banner);
 
 Vue.config.productionTip = false
 
