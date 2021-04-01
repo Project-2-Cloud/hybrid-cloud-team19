a8e217b4ed442ef976639b83e65a98e055fc6c24
MorganHolsters <64076952+MorganHolsters@users.noreply.github.com>
2021-04-01 16:49:03 +0300

    new front page

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,20 @@
 <template>
-  <div id="app">
-    
-    <NavHeader />
-    <caroussel />
-    <router-view/>
-    <iconbox/>
-
-    
-  </div>
+    <div id="app">
+        <router-view name="header"></router-view>
+        <main>
+            <fade-transition origin="center" mode="out-in" :duration="250">
+                <router-view/>
+            </fade-transition>
+        </main>
+        <router-view name="footer"></router-view>
+    </div>
 </template>
-	
 <script>
-import caroussel from "@/caroussel.vue"
-import NavHeader from "@/NavHeader.vue"
-import iconbox from "@/iconbox.vue"
+import { FadeTransition } from "vue2-transitions";
+
 export default {
   components: {
-    NavHeader,
-    caroussel,
-    iconbox
-  },
-  mounted(){
-    this.$store.commit("setUrls");
-    this.$store.dispatch("getProducts");
+    FadeTransition
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
